5d57d644c96b1e01920c18e78c48060d805b943a
Santiago <san_chan97@hotmail.com>
2023-03-04 22:10:55 -0300

    update

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 "use strict";
-exports.__esModule = true;
-var express = require("express");
-var app = express();
-var PORT = 3000;
-app.get("/hola", function (req, res) {
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const app = express();
+const PORT = 3000;
+app.get("/hola", (req, res) => {
     res.json({
-        message: "hola mundo"
+        message: "hola mundo",
     });
 });
-app.listen(PORT, function () {
+app.listen(PORT, () => {
     console.log("server corriendo en el puerto", PORT);
 });
